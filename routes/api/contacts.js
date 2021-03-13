e9c0782d00a93b1f4d7a27ddf873fa79c1dcd927
KosovMax <moskvichmax@gmail.com>
2021-03-13 22:03:42 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -108,29 +108,5 @@ router.put('/:id', validate.updateContact, async (req, res, next) => {
   }
 })
 
-// router.patch('/:id/name', async (req, res, next) => {
-//   try{
-//     const contact = await Contacts.update(req.params.id, req.body);
-
-//     if(contact){
-//       return res.json({
-//         status:'success',
-//         code: 200,
-//         data:{
-//           contact
-//         }
-//       })
-//     }else{
-//       return res.status(404).json({
-//         status:'error',
-//         code: 404,
-//         data:'Not found'
-//       })
-//     }
-   
-//   }catch(e){
-//     next(e);
-//   }
-// })
 
 module.exports = router
